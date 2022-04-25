68bb1932e7f4869442522008f4ad3ac59b018aea
Евгений <zhenya.guziy@mail.ru>
2022-04-25 22:44:46 +0300

    vk auth

--- websocketChat/websocketChat.Api/Frontend/src/pages/oAuthRedirectPage/oAuthRedirectPage.jsx
+++ websocketChat/websocketChat.Api/Frontend/src/pages/oAuthRedirectPage/oAuthRedirectPage.jsx
@@ -1,29 +1,36 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ApiMethods from "../../core/api/apiMethods";
 import { buildOAuthRequest } from './business/apiRequestBuilder';
 
 export default function OAuthRedirectPage() {
+    const navigate = useNavigate();
+
     useEffect(async () => {
         try {
             const api = new ApiMethods();
             const result = await api.vkOAuth(buildOAuthRequest());
             console.log(result);
+            navigate('/chat');
         }
         catch (ex) {
             console.log(ex);
+            navigate('/');
         }
     }, []);
     
     return (
         <div className="preloader">
-            <div className="preloader__wrap">
-                <div class="preloader__one one"></div>
-                <div class="preloader__one two"></div>
-                <div class="preloader__one three"></div>
-                <div class="preloader__one four"></div>
-                <div class="preloader__two five"></div>
-                <div class="preloader__two six"></div>
-                <div class="preloader__two seven"></div>
+            <div className="preloader__container">
+                <div className="preloader__wrap">
+                    <div className="preloader__one"></div>
+                    <div className="preloader__two"></div>
+                    <div className="preloader__three"></div>
+                    <div className="preloader__four"></div>
+                </div>
+                <div className="preloader__title">
+                    Подождите, идет авторизация...
+                </div>
             </div>
         </div>
     )
